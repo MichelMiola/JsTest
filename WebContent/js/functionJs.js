4215f9cc45fe1735f2f9f060b10d383b34c5842a
Michel <miola.michel@gmail.com>
2014-08-19 16:39:11 -0300

    commit inicial

---
+++ WebContent/js/functionJs.js
@@ -0,0 +1,13 @@
+//window.onload = function () {
+//	alert('Teste load!');
+//};
+
+function checkWinner(form, campo, winningNum) {
+	
+	var form = document.getElementById("formulario");
+	
+	console.log(form.campo1);
+	console.log(form.campo2);
+	console.log('acabou');
+	
+}
